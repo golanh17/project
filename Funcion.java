516a083ae81e9605acdde408d2ff07ff68ab7ac0
golanh17 <golanh17@gmail.com>
2016-06-30 23:34:59 +0300

    Update Funcion.java

--- Funcion.java
+++ Funcion.java
@@ -1,25 +1,26 @@
-public class Functions{
+public class Function{
 
-public static int reverseNumber(int num){
-  boolean isNegative = (num < 0) ? true : false;
-  if(isNegative){
-    num *= -1;
+  public static int reverseNumber(int num){
+    boolean isNegative = (num < 0) ? true : false;
+    if(isNegative){
+      num *= -1;
+    }
+    int result = 0;
+    while (num > 0){
+      result = result*10 + num%10;
+    }
+    if(isNegative){
+      result *= -1;
+    }
+    return result;
   }
-  int result = 0;
-  while (num > 0){
-    result = result*10 + num%10;
-  }
-  if(isNegative){
-    result *= -1;
-  }
-  return result;
-}
 
-
-public static void main(String[] args){
-  System.out.println(reverse(-123));
-  System.out.println(reverse(-1));
-  System.out.println(reverse(0));
-  System.out.println(reverse(123));
-  System.out.println(reverse(1));
+  public static void main(String[] args){
+    System.out.println(reverse(-123));
+    System.out.println(reverse(-1));
+    System.out.println(reverse(0));
+    System.out.println(reverse(1));
+    System.out.println(reverse(123));
+  }
+  
 }
